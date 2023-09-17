dc27f5f38d894b3cae36fa5c91fd49dc511d83de
alansoetikno <alansoetikno@gmail.com>
2023-09-16 21:26:20 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ from os import listdir
 from datetime import datetime
 import pandas as pd
 import re
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import re
 from openpyxl import load_workbook
